1be9aeb39ad6e2419401b443e4a9896c36e48ec4
xxjjisai <1353231305@qq.com>
2019-08-08 21:53:20 +0800

    排序删除没用的部分

--- scripts/systems/RogueRenderSortSystem.lua
+++ scripts/systems/RogueRenderSortSystem.lua
@@ -1,12 +1,5 @@
 RogueRenderSortSystem = class("RogueRenderSortSystem",System)
 
-function RogueRenderSortSystem:start(actors)
-    local actors = actors or {};
-    if not next(actors) then 
-        return 
-    end
-end
-
 function RogueRenderSortSystem:update(dt,actors)
     local actors = actors or {};
     if not next(actors) then 
@@ -17,7 +10,7 @@ function RogueRenderSortSystem:update(dt,actors)
             local a_compo_SortOrder = a:GetCompo("SortOrder");
             local b_compo_SortOrder = b:GetCompo("SortOrder");
             local a_compo_Position = a:GetCompo("Position");
-            local a_compo_Size = a:GetCompo("Size"); 
+            local a_compo_Size = a:GetCompo("Size");
             local b_compo_Position = b:GetCompo("Position");
             local b_compo_Size = b:GetCompo("Size"); 
             local ay = a_compo_Position:GetData("y") + a_compo_Size:GetData("y");
@@ -25,7 +18,7 @@ function RogueRenderSortSystem:update(dt,actors)
             if a_compo_SortOrder:GetData("nLayerIndex") == option.tbLayer.human and
             b_compo_SortOrder:GetData("nLayerIndex") == option.tbLayer.human then
                 return ay < by;
-            if a_compo_SortOrder:GetData("nLayerIndex") == option.tbLayer.humandown and
+            elseif a_compo_SortOrder:GetData("nLayerIndex") == option.tbLayer.humandown and
             b_compo_SortOrder:GetData("nLayerIndex") == option.tbLayer.humandown then
                 return ay < by;
             else 
